66dac358b8631690f5fb8fc15419e9e30cfe8c3e
phiv <>
2019-05-25 18:03:27 -0400

    initial

---
+++ src/puzzled/PuzzledController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puzzled;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+/**
+ * FXML Controller class
+ *
+ * @author https://github.com/bepuzzled
+ */
+public class PuzzledController {
+
+    /**
+     * Initializes the controller class.
+     */
+   @FXML
+    private Label label;
+   
+    public void initialize() {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+
+    
+}
