259836ba1b564716d35869f29a31a6c7123c48f9
phiv <phiv@puzzled.be>
2019-05-26 09:09:48 -0400

    menu, toolbars, actions. Issues persist with all image URLs

--- src/puzzled/Puzzled.java
+++ src/puzzled/Puzzled.java
@@ -5,6 +5,10 @@
  */
 package puzzled;
 
+import java.io.File;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,14 +20,49 @@ import javafx.stage.Stage;
  * @author https://github.com/bepuzzled
  */
 public class Puzzled extends Application {
+    private static final String banner = "Puzzled! - Logic problem solver";
+    public static final String version = "3.0.0";
+    
+    private static final Logger fLogger =
+        Logger.getLogger(Puzzled.class.getPackage().getName());
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Puzzled.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Puzzled.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        
+       
+        PuzzledController controller = (PuzzledController)loader.getController();
+        controller.setupTitleBinding(primaryStage.titleProperty(), banner, version);
+        
+        primaryStage.setMaximized(true);
     }
+    
+    public Logger getLogger(){
+        return fLogger;
+    }
+ 
+    /**
+     * Export a resource embedded into a Jar file to the local file path.
+     *
+     * @param resourceName ie.: "/SmartLibrary.dll"
+     * @return The path to the exported resource
+     * @throws Exception
+     */
+//    static public void exportResource(String resourceName) {
+//        try {
+//            URL inputUrl = Puzzled.class.getResource(resourceName);
+//            String jarFolder = new File(Puzzled.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().replace('\\', '/');
+//            File dest = new File(jarFolder + resourceName);
+//            org.apache.commons.io.FileUtils.copyURLToFile(inputUrl, dest);
+//        } catch (Exception e) {
+//                fLogger.log(Level.WARNING, "unable to extract resource file "+resourceName);
+//        }
+//    }
+    
     /**
      * @param args the command line arguments
      */
--- src/puzzled/PuzzledController.java
+++ src/puzzled/PuzzledController.java
@@ -7,26 +7,117 @@ package puzzled;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.NotificationPane;
+
+
+
 /**
  * FXML Controller class
  *
  * @author https://github.com/bepuzzled
  */
 public class PuzzledController {
+    
+        private static double zoomFactor = 1.3;
+        private static double maxZoom = 2.3;
+        private static double minZoom = 0.4;
+        private static int notificationTimer = 3000;
 
+        public enum WarningType {
+            SUCCESS ("success.png"), 
+            INFO ("info.png"), 
+            WARNING ("fail.png");
+
+            private String imageName;
+
+            WarningType(String image){
+                imageName = image;
+            }
+
+            public String getImageName() {
+                return imageName;
+            }
+        }
     /**
      * Initializes the controller class.
      */
-   @FXML
+    @FXML
     private Label label;
+    
+    @FXML
+    private AnchorPane mainGrid;
+    
+    @FXML
+    private NotificationPane nPane;
+    
+    @FXML
+    private BorderPane bPane;
+    
+    @FXML
+    private VBox clueEngineVBox;
+    
+    @FXML
+    private ToolBar toolbar;
    
+    private static final Logger fLogger =
+    Logger.getLogger(Puzzled.class.getPackage().getName());
+    
+    private String appTitle;
+    private String appVersion;
+    private StringProperty appTitleProperty = new SimpleStringProperty();
+    
+    @FXML
+    private void quit(ActionEvent event) {
+        //make sure logicProblem is not dirty!
+        Platform.exit();
+    }
+    
+    @FXML
+    private void zoomInButtonAction(ActionEvent event) {
+        
+        //if (logicProblem.get().getScale() <= maxZoom) {
+        //    logicProblem.get().setScale(logicProblem.get().getScale()*zoomFactor);
+        //} else {
+            notify(WarningType.WARNING, "Maximum zoom level reached!");
+        //}
+        //fLogger.log(Level.INFO, "scale:"+logicProblem.get().getScale());
+        fLogger.log(Level.INFO, "zooming");
+    }
+    
+    public void notify(WarningType type, String text) {
+        nPane.setGraphic(new ImageView(new Image("/icons/notification-pane/"+type.getImageName())));  
+        nPane.setText(text);
+        nPane.show();
+    }
+    
+    
+    public void setupTitleBinding(StringProperty puzzledTitleProperty, String banner, String version) {
+        puzzledTitleProperty.bind(this.appTitleProperty);
+        appTitle = banner;
+        appVersion = version;
+        this.appTitleProperty.set(banner+" - "+version);
+    }
+    
+    
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        fLogger.log(Level.INFO, "java version:" + System.getProperty("java.version"));
+        fLogger.log(Level.INFO, "javafx version:" + System.getProperty("javafx.version"));
+        //label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
 
     
